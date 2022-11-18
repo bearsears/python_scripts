b8375a8b3b3d8a55c01b3a4fa28c94f4eabe570f
bearsears <61921175+bearsears@users.noreply.github.com>
2022-11-18 09:43:33 -0800

    Add files via upload

---
+++ well_match.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  1 14:41:06 2020
+
+@author: ssuh
+"""
+
+import csv
+
+
+with open('gpsWell20191203.csv', 'r', newline='') as csvfile, open('gpsWell.csv', 'r', newline='') as csvfile1, open('gpsWell20200901.csv', 'w', newline='') as cf:
+    wte = csv.writer(cf)
+    line =  csv.reader(csvfile)
+    line1 = csv.reader(csvfile1)
+    count = 0
+    for i in line:
+        jcount = 0
+        for j in line1:
+            if i[0] == j[0]:
+                wte.writerow([j[0], j[1], j[2], j[4], i[5], i[6], i[7]])
+                break
+            else:
+                wte.writerow([j[0], j[1], j[2], j[4], j[5][6:20], '', ''])
